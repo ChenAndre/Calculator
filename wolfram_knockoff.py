64a1f0c03e4e52d54df3f7f9973fcb96824cb988
wintrsatoru <chenandre23@gmail.com>
2023-07-13 18:24:53 -0700

    new commit

--- wolfram_knockoff.py
+++ wolfram_knockoff.py
@@ -1,10 +1,8 @@
 first_num = int(input("Enter your first number:"))
-      
+
 str1 = "your first number is: "
 
 
-
-    
 print(str1 + str(first_num))
 
 firstnumber = first_num
@@ -15,15 +13,15 @@ str2 = "your second number is: "
 
 print(str2 + str(second_num))
 
-second_number = second_num    
+second_number = second_num
 
 operator = str(input("What would you like to do with these numbers?" + " "))
 
 if operator == "multiply":
-    print(first_num*second_num)
+    print(first_num * second_num)
 
 if operator == "divide":
-    print(first_num/second_num)
+    print(first_num / second_num)
 
 if operator == "add":
     print(first_num + second_num)
